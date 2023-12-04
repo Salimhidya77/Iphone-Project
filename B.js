7c42f7751b5f7a7bd11fd05228708a0be3b5b01a
Salim <129696761+Salimhidya77@users.noreply.github.com>
2023-12-04 21:29:07 +0200

    Add files via upload

---
+++ B.js
@@ -0,0 +1,16 @@
+const mg1 = document.getElementById("mg1");
+const mg2 = document.getElementById("mg2");
+const mg3 = document.getElementById("mg3");
+const img4 = document.getElementById("img0");
+
+
+mg1.addEventListener("click", (eo) => {
+  img4.src = "img1.jpg";
+});
+
+mg2.addEventListener("click", (eo) => {
+  img4.src = "1mg1.jpg";
+});
+mg3.addEventListener("click", (eo) => {
+  img4.src = "img3.jpg";
+});
